6d104b53a48a11a5b2afb67f31b2eee2ce7b799b
Unknown <dennisbunne92@gmail.com>
2017-11-07 16:17:07 +0100

    updated code

--- Code/Automatic_Blinds_ESP/Automatic_Blinds_ESP.ino
+++ Code/Automatic_Blinds_ESP/Automatic_Blinds_ESP.ino
@@ -92,12 +92,6 @@ void loop(void){
  ***********************************************************************************************************************************/
 void Forward(int steps, int step_delay){
   for(int i=0; i<=STEPS; i++){
-    if(Webserver.send(200, "text/html", HTMLpage+"<p>Blinds Stop</p>")){
-        Webserver.on("/StopBlinds", [](){
-        Webserver.send(200, "text/html", HTMLpage+"<p>Blinds Stop</p>");
-    Stop();
-  });
-    }
       digitalWrite(in1, HIGH);   digitalWrite(in2, LOW);    digitalWrite(in3, LOW);     digitalWrite(in4, LOW);     delay(step_delay);
       digitalWrite(in1, LOW);    digitalWrite(in2, HIGH);   digitalWrite(in3, LOW);     digitalWrite(in4, LOW);     delay(step_delay);
       digitalWrite(in1, LOW);    digitalWrite(in2, LOW);    digitalWrite(in3, HIGH);    digitalWrite(in4, LOW);     delay(step_delay);
